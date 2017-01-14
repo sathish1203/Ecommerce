dfc7081caeb087d405b64cac4811cd8212c898f4
sathish1203 <sathish1203@gmail.com>
2017-01-14 17:52:30 +0530

    Committing the files, the project now, 1. Has a landing page. 2. Has a seperate login and registration page.

--- Ecommerce/src/main/java/com/niit/controllers/helloController.java
+++ Ecommerce/src/main/java/com/niit/controllers/helloController.java
@@ -15,5 +15,28 @@ public class helloController{
       model.addAttribute("message", "Hello Spring MVC Framework!");
       return "hello";
    }
-
+   @RequestMapping(value = "/landing",method = RequestMethod.GET)
+   public String printLanding(ModelMap model) {
+	  System.out.println("In printLanding");
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "landing2";
+   }
+   @RequestMapping(value = "/login",method = RequestMethod.GET)
+   public String triggerLogin(ModelMap model) {
+	  System.out.println("In printLanding");
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "login";
+   }
+   @RequestMapping(value = "/signup",method = RequestMethod.GET)
+   public String triggerSignup(ModelMap model) {
+	  System.out.println("In printLanding");
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "signup";
+   }
+   @RequestMapping(value = "/logged",method = RequestMethod.GET)
+   public String triggerLogged(ModelMap model) {
+	  System.out.println("In printLanding");
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "logged";
+   }
 }
---
+++ Ecommerce/src/main/java/com/niit/controllers/landingController.java
@@ -0,0 +1,18 @@
+package com.niit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+public class landingController {
+	
+	 @RequestMapping(value = "/landing", method = RequestMethod.GET)
+	   public String printLanding(ModelMap model) {
+		  System.out.println("In printHello");
+	      model.addAttribute("message", "Hello Spring MVC Framework!");
+	      return "hello";
+	   }
+
+}
